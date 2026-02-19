7fb6548f69e91e7d7902f7903c72252615f1b02d
Alexei91912 <alexis.pimpernelle@gmail.com>
2026-02-19 01:34:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "dei-v1";
+const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).then((r) => {
+      const copy = r.clone();
+      caches.open(CACHE).then((c) => c.put(e.request, copy));
+      return r;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
